ac399bb3974e4464ec13071f230aec29574fbfb9
Dmitry Sichkar <dmmeteo@gmail.com>
2018-02-22 15:24:39 +0200

    post requst to add new user

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            users: []
+            users: [],
+            username: '',
+            email: ''
         }
     }
 
@@ -25,6 +28,27 @@ class App extends Component {
         .catch((err) => {console.log(err);})
     }
 
+    addUser(event) {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        }
+
+        axios.post(`${process.env.REACT_APP_USERS_SERVISE_URL}/users`, data)
+        .then((res) => {
+            this.getUsers();
+            this.setState({username: '', email: ''})
+        })
+        .catch((err) => {console.log(err);})
+    }
+
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    }
+
     render() {
         return (
             <div className='container'>
@@ -33,6 +57,13 @@ class App extends Component {
                         <br />
                             <h1>All Users</h1>
                         <hr /><br />
+                        <AddUser
+                            username={this.state.username}
+                            email={this.state.email}
+                            handleChange={this.handleChange.bind(this)}
+                            addUser={this.addUser.bind(this)}
+                        />
+                        <br/>
                         <UsersList users={this.state.users}/>
                     </div>
                 </div>
