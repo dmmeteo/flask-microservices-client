fd49d96650b7bce606283ac9a2fecf2de4a39a42
Dmitry Sichkar <dmmeteo@gmail.com>
2018-03-13 01:54:42 +0200

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Switch} from 'react-router-dom';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import NavBar from './components/NavBar';
 
 
 class App extends Component {
@@ -13,7 +14,8 @@ class App extends Component {
         this.state = {
             users: [],
             username: '',
-            email: ''
+            email: '',
+            title: 'FreeTestDriven'
         }
     }
 
@@ -52,28 +54,30 @@ class App extends Component {
 
     render() {
         return (
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-6'>
-                        <br />
-                        <Switch>
-                            <Route exact path='/' render={() => (
-                                <div>
-                                    <h1>All Users</h1>
-                                    <hr /><br />
-                                    <AddUser
-                                        username={this.state.username}
-                                        email={this.state.email}
-                                        handleChange={this.handleChange.bind(this)}
-                                        addUser={this.addUser.bind(this)}
-                                    />
-                                    <br/>
-                                    <UsersList users={this.state.users}/>
-                                </div>
-                            )}/>
-
-                            <Route exact path='/about' component={About}/>
-                        </Switch>
+            <div>
+                <NavBar title={this.state.title} />
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-md-6'>
+                            <br />
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1>All Users</h1>
+                                        <hr /><br />
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            handleChange={this.handleChange.bind(this)}
+                                            addUser={this.addUser.bind(this)}
+                                        />
+                                        <br/>
+                                        <UsersList users={this.state.users}/>
+                                    </div>
+                                )}/>
+                                <Route exact path='/about' component={About}/>
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
