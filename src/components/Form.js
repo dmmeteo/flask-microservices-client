e17685f791bba78456f3b9f5973deb71681e8648
Dmitry Sichkar <dmmeteo@gmail.com>
2018-03-16 23:15:17 +0200

    fixed router

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,51 +1,53 @@
 import React from 'react';
 
 const Form = (props) => {
-    <div>
-        <h1>{props.formType}</h1>
-        <hr/><br/>
-        <form onSubmit={(event) => props.handleUserFormSubmit(event)}>
-            {props.FormType === 'Register' &&
+    return (
+        <div>
+            <h1>{props.formType}</h1>
+            <hr/><br/>
+            <form onSubmit={(event) => props.handleUserFormSubmit(event)}>
+                {props.formType === 'Register' &&
+                    <div className="form-group">
+                        <input 
+                            name='username'
+                            className='form-control input-lg'
+                            placeholder='Enter a username'
+                            required
+                            value={props.formData.username}
+                            onChange={props.handleFormChange}
+                        />
+                    </div>
+                }
                 <div className="form-group">
-                    <input 
-                        name='username'
-                        className="form-control input-lg"
-                        placeholder="Enter a username"
+                    <input
+                        name='email'
+                        className='form-control input-lg'
+                        type='email'
+                        placeholder='Enter an email addres'
                         required
-                        value={props.formData.username}
+                        value={props.formData.email}
                         onChange={props.handleFormChange}
                     />
                 </div>
-            }
-            <div className="form-group">
+                <div className="form-group">
+                    <input
+                        name='password'
+                        className='form-control input-lg'
+                        type='password'
+                        placeholder='Enter a password'
+                        required
+                        value={props.formData.password}
+                        onChange={props.handleFormChange}
+                    />
+                </div>
                 <input
-                    name='email'
-                    className='form-control import-lg'
-                    type='email'
-                    placeholder='Enter an email addres'
-                    required
-                    value={props.formData.email}
-                    onChange={props.handleFormChange}
+                    type="submit"
+                    className="btn btn-primary btn-lg btn-block"
+                    value="Submit"
                 />
-            </div>
-            <div className="form-group">
-                <input
-                    name='password'
-                    className='form-control import-lg'
-                    type='password'
-                    placeholder='Enter a password'
-                    required
-                    value={props.formData.password}
-                    onChange={props.handleFormChange}
-                />
-            </div>
-            <input
-                type="submit"
-                className="btn btn-primary btn-lg btn-block"
-                value="Submit"
-            />
-        </form>
-    </div>
+            </form>
+        </div>
+    )
 }
 
 export default Form
